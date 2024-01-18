44a9c2a5407fb4c89b894714866159295d3db543
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2024-01-18 11:55:01 +0545

    Resume Link Updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = (props) => {
   return (
     <button className={props.className}>
       <a
-        href="https://drive.google.com/file/d/1Y0JHDejLaVROOPFeUFMjC2QiM_xwMNOX/view?usp=sharing"
+        href="https://drive.google.com/file/d/1z4Ug98lPW49hGmV4eZafxsjcR7ILNRkH/view?usp=sharing"
         target="_blank"
         className="bg-gradient-to-r from-cyan-500 to-teal-500 bg-cyan-500 text-white px-10 py-2 rounded-md text-sm md:text-base md:px-4"
       >
